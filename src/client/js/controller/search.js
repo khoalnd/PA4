129c53b832efb0a639b643e2a1fcafa17de53929
Đinh Huy <quochuy00z@gmail.com>
2018-06-02 01:30:26 +0700

    Commit AutoComplete

--- src/client/js/controller/search.js
+++ src/client/js/controller/search.js
@@ -1,13 +1,23 @@
+"use strict";
 app.controller("searchCtrl", ['$scope', '$rootScope', '$http', 'firebase', '$firebaseObject', searchCtrl]);
 function searchCtrl($scope, $rootScope, $http, firebase, $firebaseObject) {
 	function init() {
 		$scope.listLyrics = [];
+		$scope.listAutoComplete = [];
+
 		var url = new URL(window.location.href.replace("#!", ""));
 		var songKey = url.searchParams.get("keywords");
 
-		firebase.orderByChild('SongName').equalTo(songKey).once("value", (snap) => {
+		firebase.orderByChild('SongName').once("value", (snap) => {
 			snap.forEach((childSnap) => {
-				$scope.listLyrics.push({
+				if (childSnap.val().SongName == songKey) {
+					$scope.listLyrics.push({
+						key: childSnap.key,
+						data: childSnap.val(),
+					});
+				}
+
+				$scope.listAutoComplete.push({
 					key: childSnap.key,
 					data: childSnap.val(),
 				});
@@ -17,26 +27,26 @@ function searchCtrl($scope, $rootScope, $http, firebase, $firebaseObject) {
 
 	init();
 
-	// $scope.complete = function(string){  
- //       	$scope.hidethis = false;  
- //       	var output = [];  
- //       	angular.forEach($scope.listLyrics, function(itemp){  
- //            if(itemp.data.toLowerCase().indexOf(string.toLowerCase()) >= 0)  
- //            {  
- //                 output.push(txtSearch);  
- //            }  
- //       	});  
- //       	$scope.filterCountry = output;  
- //  	}
+	$scope.complete = function(string) {
+		$scope.nHide = false;
+        var output = [];
 
-  	// $scope.fillTextbox = function(string){  
-   //         $scope.txtSearch = string;  
-   //         $scope.hidethis = true;  
-   //  }  
+	    angular.forEach($scope.listAutoComplete, function(iTemp) {
+	        if(iTemp.data.SongName.toLowerCase().indexOf(string.toLowerCase()) >= 0) {
+	        	output.push(iTemp.data.SongName);
+	        }
+	    });
 
-	$scope.search = function() {
+	    $rootScope.filterLyrics = output;
+   	}
+
+	$scope.search = function(string) {
+		$scope.nHide = true;
+		
+		if (string) {
+			$scope.txtSearch = string;
+		}
+
 		window.top.location = '#!/search?keywords=' + $scope.txtSearch;
 	}
-
-
 }
